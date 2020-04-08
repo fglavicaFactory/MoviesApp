d1f216605424cea98d7fd62188af92744cad1f82
Filip Glavica <filip.glavica@factory.hr>
2020-04-08 14:48:28 +0200

    Initial Commit

---
+++ MovieApplicationFG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApplicationFG
+//
+//  Created by Filip Glavica on 08/04/2020.
+//  Copyright © 2020 Filip Glavica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
